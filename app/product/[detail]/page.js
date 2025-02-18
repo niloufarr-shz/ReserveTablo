54e99676271ca353e6467bb614e43538977f438f
niloufar <niloufarshahyanzadeh@gmail.com>
2025-02-18 04:07:36 -0800

    data fake

---
+++ app/product/[detail]/page.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Tablo from "@/components/data/Tablo";
+import L from 'leaflet';
+
+function detail({ params }) {
+  const detail = Tablo.filter((nil) => nil.id == params.detail);
+  return (
+
+    <div>
+      {detail.map((ana) => (
+        <div key={ana.id}>
+        <img src={ana.avatar} alt="" />
+
+        <p className="bg-indigo-300">{ana.city}</p>
+        <p>{ana.address}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default detail;
