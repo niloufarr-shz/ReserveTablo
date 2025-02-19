df83191ffdd4b67fa0f144492942f4786600c8c3
mohammad77hastam@gmail.com <mohammad77hastam@gmail.com>
2025-02-19 14:20:13 +0330

    edite

--- app/components/MainFile/SelectCity.js
+++ app/components/MainFile/SelectCity.js
@@ -1,10 +1,9 @@
-import React from "react";
 import Mydata from "../main/Mydata";
 import Tablo from "@/components/data/Tablo";
 function SelectCity() {
   return (
     <div dir="rtl">
-      <div className=" flex flex-col   w-[95%] h-[264px] bg-slate-300 m-auto ">
+      <div className=" flex flex-col  w-[95%] h-[264px] bg-slate-300 m-auto ">
         <form className="  flex flex-col items-center justify-around w-[100%] h-[264px] ">
           <label className=" pt-4 mb-2 text-base font-medium text-gray-900 dark:text-white">
             انتخاب شهر
@@ -18,7 +17,9 @@ function SelectCity() {
             انتخاب رسانه
           </label>
           <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[300px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option value=""></option>
+            {Tablo.map((mamad) => (
+              <option key={mamad.id}>{mamad.mediatype}</option>
+            ))}
           </select>
           <div className="m-auto flex items-center justify-center text-base text-gray-900 bg-slate-400 w-[200px] h-[40px] rounded-md ">
             <button> جستجو بیلبورد </button>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        vazir: "vazir",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
