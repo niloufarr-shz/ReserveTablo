bf663b78a12e798b13c94198de25d47b108580e8
hamidreza soltani <h.soltani8452@gmail.com>
2025-02-22 14:13:24 +0330

    mammad 12/4

--- components/MainFile/SelectCity.js
+++ components/MainFile/SelectCity.js
@@ -1,33 +1,61 @@
+"use client";
+import { useState } from "react";
 import Mydata from "../main/Mydata";
 import Tablo from "../data/Tablo";
 function SelectCity() {
+  const [selectedCity, setSelectedCity] = useState("");
+  const [selectedMediaType, setSelectedMediaType] = useState("");
+  const [filteredData, setFilteredData] = useState(Tablo);
+  function handleCityChange(e) {
+    setSelectedCity(e.target.value);
+  }
+  function handleMediaChange(e) {
+    setSelectedMediaType(e.target.value);
+  }
+
   return (
-    <div dir="rtl" className="mt-16">
+    <div dir="rtl">
       <div className=" flex flex-col  w-[95%] h-[264px] bg-slate-300 m-auto ">
-        <form className="  flex flex-col items-center justify-around w-[100%] h-[264px] ">
+        <div className="  flex flex-col items-center justify-around w-[100%] h-[264px] ">
           <label className=" pt-4 mb-2 text-base font-medium text-gray-900 dark:text-white">
             انتخاب شهر
           </label>
-          <select className="  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[300px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            {Tablo.map((mamad) => (
-              <option key={mamad.id}>{mamad.city}</option>
-            ))}
+          <select
+            value={selectedCity}
+            onChange={handleCityChange}
+            className="  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[300px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            {Array.from(new Set(Tablo.map((mamad) => mamad.city))).map(
+              (uniqueCity) => (
+                <option value={uniqueCity} key={uniqueCity}>
+                  {uniqueCity}
+                </option>
+              )
+            )}
           </select>
           <label className=" pt-4 mb-2 text-base font-medium text-gray-900 dark:text-white">
             انتخاب رسانه
           </label>
-          <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[300px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            {Tablo.map((mamad) => (
-              <option key={mamad.id}>{mamad.mediatype}</option>
-            ))}
+          <select
+            value={selectedMediaType}
+            onChange={handleMediaChange}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[300px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            {Array.from(new Set(Tablo.map((mamad) => mamad.mediatype))).map(
+              (uniqueMediatype) => (
+                <option key={uniqueMediatype} value={uniqueMediatype}>
+                  {uniqueMediatype}
+                </option>
+              )
+            )}
           </select>
-          <div className="m-auto flex items-center justify-center text-base text-gray-900 bg-slate-400 w-[200px] h-[40px] rounded-md ">
-            <button> جستجو بیلبورد </button>
-          </div>
-        </form>
+        </div>
       </div>
 
-      <Mydata />
+      <Mydata
+        selectedCity={selectedCity}
+        selectedMediaType={selectedMediaType}
+      />
     </div>
   );
 }
--- components/main/Mydata.js
+++ components/main/Mydata.js
@@ -8,44 +8,66 @@ import Tablo from "../data/Tablo";
 import Link from "next/link";
 import Image from "next/image";
 
-function Mydata() {
-  const filteredData = Tablo.filter((mamad) => mamad.province === "تهران");
-  const lastThree = filteredData.slice(-5);
+function Mydata({ selectedCity, selectedMediaType }) {
+  const filteredData = Tablo.filter(
+    (mamad) =>
+      mamad.city === selectedCity && mamad.mediatype === selectedMediaType
+  );
+
+  const last = filteredData.slice(-10);
 
   return (
     <div className="w-[95%] m-auto">
-      <Swiper
-        spaceBetween={10}
-        slidesPerView={1}
-        freeMode={true}
-        grabCursor={true}
-        autoplay={false}
-        breakpoints={{
-          320: { slidesPerView: 1 },
-          640: { slidesPerView: 2 },
-          700: { slidesPerView: 3 },
-          1024: { slidesPerView: 4 },
-          1280: { slidesPerView: 5 },
-          1536: { slidesPerView: 5 },
-        }}
-      >
-        {lastThree.map((mamad) => (
-          <SwiperSlide key={mamad.id}>
-            <Link href={`/product/${mamad.id}`}>
-              <div className="flex flex-col items-center">
-                <Image
-                  src={mamad.avatar}
-                  alt=""
-                  width={500}
-                  height={500}
-                  className="mt-2 w-[239px] h-[239px] rounded-lg"
-                />
-                <p>{mamad.address}</p>
-              </div>
-            </Link>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      {last.length === 0 ? (
+        <div className="w-[350px] h-[70px] m-auto mt-5 ">
+          <p className="text-center text-[24px] border text-red-700 border-black ">
+            موردی یافت نشد
+          </p>
+        </div>
+      ) : (
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          freeMode={true}
+          grabCursor={true}
+          autoplay={false}
+          breakpoints={{
+            320: { slidesPerView: 1 },
+            640: { slidesPerView: 2 },
+            700: { slidesPerView: 3 },
+            1024: { slidesPerView: 4 },
+            1280: { slidesPerView: 5 },
+            1536: { slidesPerView: 5 },
+          }}
+        >
+          {last.map((mamad) => (
+            <SwiperSlide key={mamad.id}>
+              <Link href={`/product/${mamad.id}`}>
+                <div className="flex flex-col items-center">
+                  <Image
+                    src={mamad.avatar}
+                    alt={mamad.mediatype}
+                    width={500}
+                    height={500}
+                    className="mt-2 w-[239px] h-[239px] rounded-md"
+                  />
+
+                  <p className="px-2">{mamad.address}</p>
+
+                  <div className="flex justify-between w-[230px]  ">
+                    <div className="border  border-slate-700 px-1  rounded-md ">
+                      <p>{mamad.mediatype}</p>
+                    </div>
+                    <div className="border border-slate-700 px-1 rounded-md ">
+                      <p>{mamad.city}</p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
     </div>
   );
 }
